621d987cae508ea6db5d3bf53f306dcc77123faf
Judd Arias <Juddarias@gmail.com>
2021-11-26 15:16:01 -0800

    feat: add badge to display amount of unread messages per conversation and bold preview text if conversation has unread messages

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Badge, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    justifyContent: "space-between",
     marginLeft: 20,
+    display: "flex",
     flexGrow: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   username: {
     fontWeight: "bold",
@@ -17,6 +18,14 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 12,
     color: "#9CADC8",
     letterSpacing: -0.17,
+    fontWeight: 500,
+  },
+  unreadText: {
+    color: "black",
+  },
+  badge: {
+    marginRight: 20,
+    backgroundColor: theme.palette.primary.main,
   },
 }));
 
@@ -24,7 +33,7 @@ const ChatContent = (props) => {
   const classes = useStyles();
 
   const { conversation } = props;
-  const { latestMessageText, otherUser } = conversation;
+  const { latestMessageText, otherUser, unreadMessages } = conversation;
 
   return (
     <Box className={classes.root}>
@@ -32,10 +41,20 @@ const ChatContent = (props) => {
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
-        <Typography className={classes.previewText}>
+        <Typography
+          className={`${classes.previewText} ${
+            unreadMessages > 0 && classes.unreadText
+          }
+          }`}
+        >
           {latestMessageText}
         </Typography>
       </Box>
+      <Badge
+        className={classes.badge}
+        badgeContent={unreadMessages}
+        color="primary"
+      />
     </Box>
   );
 };
